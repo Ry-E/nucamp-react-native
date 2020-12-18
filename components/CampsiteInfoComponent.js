d091b413510f2dc96e6bf33f0864ada09f710c11
Ryan Eldon <47878230+rdogstyle@users.noreply.github.com>
2020-12-18 03:04:13 -0500

    gestures part 2 extended

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -27,10 +27,18 @@ function RenderCampsite(props) {
 
     const { campsite } = props;
 
+    const view = React.createRef();
+
     const recognizeDrag = ({ dx }) => (dx < -200) ? true : false;
+
+    const recognizeComment = ({ dx }) => (dx > -200) ? true : false;
 
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
+        onPanResponderGrant: () => {
+            view.current.rubberBand(1000)
+                .then(endState => console.log(endState.finished ? 'finished' : 'canceled'));
+        },
         onPanResponderEnd: (e, gestureState) => {
             console.log('pan responder end', gestureState);
             if (recognizeDrag(gestureState)) {
@@ -51,6 +59,8 @@ function RenderCampsite(props) {
                     ],
                     { cancelable: false }
                 );
+            } else if (recognizeComment(gestureState)) {
+                props.onShowModal()
             }
             return true;
         }
@@ -58,7 +68,12 @@ function RenderCampsite(props) {
 
     if (campsite) {
         return (
-            <Animatable.View animation='fadeInDown' duration={2000} delay={1000} {...panResponder.panHandlers}>
+            <Animatable.View
+                animation='fadeInDown'
+                duration={2000}
+                delay={1000}
+                ref={view}
+                {...panResponder.panHandlers}>
                 <Card
                     featuredTitle={campsite.name}
                     image={{ uri: baseUrl + campsite.image }} >
